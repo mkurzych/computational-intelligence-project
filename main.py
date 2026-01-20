b34198608925b1c67c7d7d93b93d0e52f46ec1b9
mkurzych <marta.kurzych@wp.pl>
2026-01-20 22:12:44 +0100

    feat: implemented nearest neighbour heuristic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from generator import Generator, plot_graph
 from christofides.christofides import christofides
 from brute_force.brute_force import brute_force
+from nearest_neighbour.nearest_neighbour import nearest_neighbour
+
 
 nodes = 6
 gen = Generator(nodes)
@@ -18,6 +20,14 @@ print()
 
 print("+++ Using Christofides Algorithm +++")
 weight, path = christofides(graph, source_node)
+print("Approximate TSP path:", path)
+print("Total weight of the path:", weight)
+plot_graph(graph, path=[(path[i], path[i + 1]) for i in range(len(path) - 1)] + [(path[-1], path[0])])
+
+print()
+
+print("+++ Using Nearest Neighbour Heuristic +++")
+weight, path = nearest_neighbour(graph, source_node)
 print("Approximate TSP path:", path)
 print("Total weight of the path:", weight)
 plot_graph(graph, path=[(path[i], path[i + 1]) for i in range(len(path) - 1)] + [(path[-1], path[0])])
---
+++ nearest_neighbour/nearest_neighbour.py
@@ -0,0 +1,32 @@
+
+
+def nearest_neighbour(graph, start):
+    visited = set()
+    path = []
+    current = start
+    weight = 0
+
+    while len(visited) < len(graph):
+        visited.add(current)
+        path.append(current)
+
+        nearest = None
+        nearest_distance = float('inf')
+
+        for neighbour in graph[current].keys():
+            if neighbour not in visited and graph[current][neighbour]['weight'] < nearest_distance:
+                nearest = neighbour
+                nearest_distance = graph[current][neighbour]['weight']
+
+        if nearest is None:
+
+            break
+
+        current = nearest
+        weight += nearest_distance
+
+    weight += graph[current][start]['weight']
+    index = path.index(start)
+    path = path[index:] + path[:index] + [path[index]]
+
+    return weight, path
